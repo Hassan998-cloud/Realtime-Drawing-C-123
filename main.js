6f25a83b0ad66829e4e69144c352a64535de425c
Hassan998-cloud <81471046+Hassan998-cloud@users.noreply.github.com>
2021-11-15 19:50:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -17,9 +23,21 @@ function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log('noseX = ' + noseX + 'noseY =' + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log('leftWristX =' + leftWristX + 'rightWristX =' + rightWristX + 'difference =' + difference);
     }
 }
 
 function draw(){
     background("#00c7eb");
+    document.getElementById("drawing").innerHTML = 'Width and Height of a square will be = ' + difference + 'px';
+    fill("#03fcf8");
+    stroke("#fcf003");
+    square(noseX,noseY,difference);
 }
